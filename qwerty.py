f009ed13809746d759d78442342f97bb0cd76fff
unknown <qwery0621@gmail.com>
2024-05-04 19:23:09 +0500

    qwerty

--- qwerty.py
+++ qwerty.py
@@ -1,6 +1,12 @@
-a = 15
-if a > 10 and a < 20:
-    print("True")
+# a = 15
+# if a > 10 and a < 20:
+#     print("True")
 
+# else:
+#     print("False")
+
+a = ['a','b','c','d','e']
+if len(a) < 6 and len(a) > 0:
+    print("True")
 else:
     print("False")
